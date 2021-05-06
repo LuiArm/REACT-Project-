2222213e062a72fce7c345e05ab7ae450b96ecc5
luis armendariz <luisarmendariz@luiss-MacBook-Pro.local>
2021-05-05 21:12:55 -0600

    added carousel to front page, needs work

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {Route,Switch} from 'react-router-dom'
 import Restaurants from "./pages/Restaurants"
 import Nav from './components/Nav'
@@ -9,6 +10,7 @@ import Restaurant from './components/Restaurant'
 
 function App() {
   return (
+    //  style={{backgroundImage: url`https://res.cloudinary.com/dr6sahlrw/image/upload/v1620259248/pablo-merchan-montes-SCbq6uKCyMY-unsplash_q574js.jpg`}}
     <div className="App">
       <Nav />
       <Switch>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,53 @@
 import React from 'react';
-
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import Carousel from 'react-bootstrap/Carousel'
 
 const Main = (props) => {
-    return <h1>Main page</h1>;
+    return(
+        <Container fluid="md" style={{padding: 0 }}>
+             {/* <h1>Main page</h1>  
+             <Button>Cheese</Button> */}
+             <Carousel fade className="carousel">
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="https://res.cloudinary.com/dr6sahlrw/image/upload/v1620259248/pablo-merchan-montes-SCbq6uKCyMY-unsplash_q574js.jpg"
+                    alt="First slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+
+                    src="https://res.cloudinary.com/dr6sahlrw/image/upload/v1620259472/tijana-drndarski-9xgHnuy45Ks-unsplash_hzqhjb.jpg"
+                    alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="https://res.cloudinary.com/dr6sahlrw/image/upload/v1620259232/rodion-kutsaev-EPwuZxdketc-unsplash_juuiij.jpg"
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                </Carousel>
+        </Container>
+    )   
 }
 
 export default Main
